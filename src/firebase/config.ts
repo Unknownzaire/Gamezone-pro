80a8078507f196811923883003f070de7068a9cb
Unknown Zaire <unknownzaire@gmail.com>
2026-02-13 12:06:51 +0000

    chang api key add this AIzaSyA8vChw2BU8iLP5irHTc8MRrXQQialFiDg

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "gamezone-pro-38727990-3ba36",
   "appId": "1:130911994597:web:09be35b4417d2874d28a0c",
-  "apiKey": "AIzaSyBFrzX6ZZrHP5SSPpsk79Si5OzVLt-a9XM",
+  "apiKey": "AIzaSyA8vChw2BU8iLP5irHTc8MRrXQQialFiDg",
   "authDomain": "gamezone-pro-38727990-3ba36.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "130911994597"
