834a6b290b177761cb796620f086702f73b459a5
Unknown Zaire <unknownzaire@gmail.com>
2026-04-21 13:27:39 +0000

    Make the following changes:
    Find this element:
    
      <span class="text-sm

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import React, { useState, useEffect } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -682,11 +681,6 @@ export default function ProfilePage() {
                 <Input id="new-password" type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
               </div>
               <Button onClick={handleChangePassword} className="w-full">Change Password</Button>
-              <div className="text-center pt-2">
-                <Link href="/forgot-password">
-                    <span className="text-sm text-muted-foreground hover:text-primary">Forgot your password?</span>
-                </Link>
-              </div>
           </CardContent>
         </Card>
         
